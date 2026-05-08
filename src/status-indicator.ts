fee257cd15dfb7621eb83ff2053f0f4756d9da51
Ayo <ayo@ayco.io>
2026-05-08 13:25:14 +0200

    feat: set up status-indicator compontnt

--- src/status-indicator.ts
+++ src/status-indicator.ts
@@ -1,9 +1,11 @@
 import { WebComponent, html } from 'web-component-base'
 
-class HelloWorld extends WebComponent {
+class StatusIndicator extends WebComponent {
     get template(): any {
         return html`
             <h1>Hello, World!</h1>
         `
     }
 }
+
+customElements.define('status-indicator', StatusIndicator)
