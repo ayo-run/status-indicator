7e8cdee7df53662dbed9883463eb0d8c14dba133
Ayo <ayo@ayco.io>
2026-05-08 19:55:33 +0200

    chore: separate build:lib script

---
+++ vite-lib.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(import.meta.dirname, 'src/status-indicator.js'),
+            name: 'StatusIndicator',
+            fileName: 'status-indicator'
+        }
+    }
+})
