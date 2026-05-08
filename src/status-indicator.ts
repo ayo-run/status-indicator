c410f0775bcc816179be54868981bc50f37d10ad
Ayo <ayo@ayco.io>
2026-05-08 17:47:33 +0200

    feat: implement status property

--- src/status-indicator.ts
+++ src/status-indicator.ts
@@ -1,11 +1,34 @@
 import { WebComponent, html } from 'web-component-base'
-import stylesheet from './stylesheets'
-
-console.log(stylesheet.cssRules)
 
 class StatusIndicator extends WebComponent {
+    static shadowRootInit: ShadowRootInit = {
+        mode: 'closed'
+    }
+
+    static props = {
+        status: 'default',
+    }
+
+    #indicatorColor: any = {
+        default: [216, 226, 233],
+        active: [0, 149, 255],
+        positive: [75, 210, 143],
+        intermediary: [255, 170, 0],
+        negative: [255, 77, 77]
+    }
+
     get template(): any {
         return html`
+            <div
+                style=${{
+                display: 'inline-block',
+                borderRadius: '50%',
+                cursor: 'pointer',
+                width: '10px',
+                height: '10px',
+                backgroundColor: `rgba(${this.#indicatorColor[this.props.status]})`
+            }}
+            ></div>
             <slot></slot>
         `
     }
